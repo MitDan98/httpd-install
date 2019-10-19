598662e21bbab90887394374ac83fc4cb6faef43
MitDan98 <56722622+MitDan98@users.noreply.github.com>
2019-10-20 00:17:24 +0300

    Create httpdinstall.sh

---
+++ httpdinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Author: Mititi Dan
+#Mail: danmititi@gmail.com
+yum install httpd
+if [ $? -ne 0 ]; then  
+ echo "Cannot install httpd Exiting..."
+	exit 1
+else
+	echo "httpd has been installed."
+fi
